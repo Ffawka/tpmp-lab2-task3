80e29932c8602fd9969ee7899209f524b4d04811
Ffawka <miss94961@gmail.com>
2026-03-19 18:14:31 +0300

    Create twins.h

---
+++ twins/twins.h
@@ -0,0 +1,10 @@
+#ifndef TWINS_H
+#define TWINS_H
+
+// Функция проверки, является ли число простым
+int isPrime(int n);
+
+// Функция поиска и вывода пар чисел-близнецов
+void findTwinPrimes(int limit);
+
+#endif
